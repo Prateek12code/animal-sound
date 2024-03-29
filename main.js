d4f081ff4e1808f3c54aca99fac10499cd08e018
PrateekShukla <124058592+Prateek12code@users.noreply.github.com>
2024-03-29 14:52:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 function start(){
     navigator.mediaDevices.getUserMedia({audio: true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/fBw4IU00I/model.json', modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/CQx54c3c4/model.json', modelReady);
 }
 
 function modelReady(){
     classifier.classify(gotResults);
+}
+
+function gotResults(error, results){
+    if (error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'I Can Hear - '+results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - '+(results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+
+        img = document.getElementById('cat');
+        img1 = document.getElementById('dog');
+
+        if(results[0].label == "cat"){
+            img.src = 'cat.gif';
+            img1.src = 'dog.webp';
+        }else if(results[0].label == "dog"){
+            img.src = 'cat.webp';
+            img1.src = 'dog.gif';
+        }else{
+            img.src = 'cat.webp';
+            img1.src = 'dog.webp';
+        }
+    }
 }
